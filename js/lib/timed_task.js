9dad3aec577f0ce7f4257d6a763e2b5ce265d18c
Lucas <lucashen@gmail.com>
2015-08-30 14:04:58 -0300

    create tasks with Jasmine

---
+++ js/lib/timed_task.js
@@ -0,0 +1,8 @@
+var TimedTask = function(options) {
+
+    options = options || {};
+    var that = new Task(options);
+    that.startingDate = options.startingDate || new Date().getTime();
+
+    return that;
+};
