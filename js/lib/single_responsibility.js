181e89a761b40897b988ed27cbd79c52a9b8ff8a
Lucas <lucashen@gmail.com>
2015-09-05 15:42:37 -0300

    create test single responsibility

--- js/lib/single_responsibility.js
+++ js/lib/single_responsibility.js
@@ -1,8 +1,12 @@
 var Liker = function() {
+    var that = this;
+    that.like = 1;
+
     $("#like").on("click", function(e) {
         e.preventDefault();
 
-        $(this).html(parseInt($(this).html()) + 1);
+        that.like++;
+        $(this).html(that.like);
 
         $("#like_count").append("<li>"+new Date().toString()+"</li>");
     });
