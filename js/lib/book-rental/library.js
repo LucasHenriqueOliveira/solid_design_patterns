5ee783d6b7415f835162272db57c57b29cd499c2
Lucas <lucashen@gmail.com>
2015-09-08 16:25:33 -0300

    include the library

---
+++ js/lib/book-rental/library.js
@@ -0,0 +1,11 @@
+var Library = function() {};
+
+Library.decrementAvailabilityFor = function(book) {
+    book._availability--;
+};
+
+Library.incrementAvailabilityFor = function(book) {
+    book._availability++;
+};
+
+Library.allOrders = function() {};
